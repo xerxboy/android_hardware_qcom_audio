c63a7ba28a883b55370636b9e773b735cba8230c
Chirayu Desai <chirayudesai1@gmail.com>
2017-12-19 15:32:35 -0500

    Use kernel headers

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -51,6 +51,9 @@ LOCAL_C_INCLUDES := \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
         $(call include-path-for, audio-effects)
 
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -111,6 +114,9 @@ LOCAL_C_INCLUDES := \
         $(call include-path-for, audio-route) \
         $(call project-path-for,qcom-audio)/hal/audio_extn \
         external/tinycompress/include
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 include $(BUILD_SHARED_LIBRARY)
 
